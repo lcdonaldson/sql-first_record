84a3e6a2768fde437cf9f6037b6022de89381322
lcdonaldson <lcdbacanas@gmail.com>
2016-08-23 13:16:18 -0500

    Update home_teaching.sql

--- home_teaching.sql
+++ home_teaching.sql
@@ -1,11 +1,17 @@
+CREATE TABLE home_teacher (
+id int,
+name varchar (255),
+age int,
+companion varchar(255)
+);
+
+INSERT INTO home_teaching (comapanion)
+VALUES('Raymond');
+    
 SELECT 
   id, 
   name, 
   age, 
   companion
-FROM home_teaching
+FROM home_teacher
 WHERE age > 30;
-
-INSERT INTO home_teaching (comapanion)
-VALUES('Raymond');
-    

0d90e94f5a9c0dfa2f070888b2121cf849dc6268
lcdonaldson <lcdbacanas@gmail.com>
2016-08-24 21:02:43 -0500

    one delete command added

--- home_teaching.sql
+++ home_teaching.sql
@@ -5,13 +5,24 @@ age int,
 companion varchar(255)
 );
 
-INSERT INTO home_teaching (comapanion)
-VALUES('Raymond');
+INSERT INTO home_teaching (id, name, age, comapanion)
+VALUES(1, 'Raymond', 43, 'Steve');
+
+INSERT INTO home_teaching (id, name, age, comapanion)
+VALUES(2, 'Jace', 25, 'Carl');
+
+INSERT INTO home_teaching (id, name, age, comapanion)
+VALUES(3, 'Mike', 51, 'James');
+
+INSERT INTO home_teaching (id, name, age, comapanion)
+VALUES(4, 'John', 33, 'Eric');
+
+INSERT INTO home_teaching (id, name, age, comapanion)
+VALUES(5, 'Ryan', 37, 'Dee');
     
 SELECT 
-  id, 
-  name, 
-  age, 
-  companion
+  id, name, age, companion
 FROM home_teacher
 WHERE age > 30;
+
+DELETE FROM home_teacher WHERE id = 5;
